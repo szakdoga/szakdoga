9942dbf4a975df9c63cae94aed2cb1954d9c5efe
evingerbono <bono0823@gmail.com>
2024-01-23 15:18:59 +0100

    tábla_Controller

--- backend/szakdoga/app/Http/Controllers/AdminController.php
+++ backend/szakdoga/app/Http/Controllers/AdminController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Admin;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     //
+    public function index()
+    {
+        return Admin::all();
+    }
+
+    public function store(Request $request)
+    {
+        $admin = new Admin();
+        $admin ->userId = $request->userId;
+        $admin->save();
+
+        return $admin;
+    }
+
+    public function show($id)
+    {
+        return Admin::find($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $admin  = Admin::find($id);
+        if ($admin) {
+            $admin = new Admin();
+            $admin ->userId = $request->userId;
+            
+            $admin ->save();
+        }
+
+        return $$admin;
+    }
+
+    public function destroy($id)
+    {
+        $admin = Admin::find($id);
+        if ($admin ) {
+            $admin ->delete();
+        }
+    }
 }
