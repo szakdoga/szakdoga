e7336ed9dd69e494d3a47ad14675d9eea90d8a85
Erno1234 <palocierno12@gmail.com>
2024-01-30 15:15:30 +0100

    thunder tesztelés, route javítások

--- backend/szakdoga/database/migrations/2024_01_05_113821_create_szaks_table.php
+++ backend/szakdoga/database/migrations/2024_01_05_113821_create_szaks_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Szak;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -18,6 +19,15 @@ return new class extends Migration
             $table->string('megnevezes');
             $table->timestamps();
         });
+
+
+        Szak::create([
+            'megnevezes' => "Szoftver", 
+        ]);
+
+        Szak::create([
+            'megnevezes' => "IRU", 
+        ]);
     }
 
     /**
--- backend/szakdoga/routes/web.php
+++ backend/szakdoga/routes/web.php
@@ -29,7 +29,7 @@ Route::get('/', function () {
 Route::get('/szakok', [SzakController::class, 'index']);
 Route::post('/szakok/create', [SzakController::class, 'store']);
 Route::get('/szakok/{id}', [SzakController::class, 'show']);
-Route::put('/szakok/{id}/edit', [SzakController::class, 'update']);
+Route::put('/szakok/edit/{id}', [SzakController::class, 'update']);
 //Jogosultsag
 Route::get('/jogosultsagok', [JogosultsagController::class, 'index']);
 Route::post('/jogosultsagok/create', [JogosultsagController::class, 'store']);
