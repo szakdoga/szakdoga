2185bedd1c3e048161f11f4a6c729ec03c62bd18
evingerbono <bono0823@gmail.com>
2024-02-27 14:27:18 +0100

    bejelentController_javítása

--- frontend/Controller/bejelntController.js
+++ frontend/Controller/bejelntController.js
@@ -1,20 +1,21 @@
 import bejelntModel from "../Model/bejelntModel.js";
 import BejelentkezesView from "../View/bejelntView.js";
-class BejelntkezesController{
+class BejelntkezesController {
     constructor() {
         const adat = { username: "", jelszo: "" };
         const model = new bejelntModel(adat);
         const szuloElem = document.querySelector(".bejelentkezes");
         console.log(szuloElem)
         const view = new BejelentkezesView(model.getAdat(), szuloElem);
-        view.submitElem.addEventListener("click", this.bejelentkezes.bind(this, model));
+        window.addEventListener("bejelentkezes", this.bejelentkezes.bind(this, model));
     }
 
     bejelentkezes(model, event) {
         event.preventDefault();
-        const adat = model.getAdat();
+        const adat = event.detail;
         console.log("Felhasználónév:", adat.username);
         console.log("Jelszó:", adat.jelszo);
     }
 }
+
 export default BejelntkezesController;
