543120fb67abbccea96396487ce595744199093b
Erno1234 <palocierno12>
2024-05-06 14:48:21 +0200

    Kovetelmeny szűrés

--- frontend/View/AdminKovetelmenyView.js
+++ frontend/View/AdminKovetelmenyView.js
@@ -20,8 +20,39 @@ class AdminKovetelmenyView {
     for (const key in this.#adat) {
       new TablaSorView(this.#adat[key], this.tableElem);
     }
+    this.keresoMenu();
   }
 
+  keresoMenu() {
+  
+    const egyediCegNevek = new Set();
+    for (const key in this.#adat) {
+      egyediCegNevek.add(this.#adat[key].neve);
+    }
+  
+    const valaszto = $('<select class="form-select" id="searchDropdown"></select>');
+    valaszto.append('<option value="">Összes</option>');
+    for (const nev of egyediCegNevek) {
+      valaszto.append(`<option value="${nev}">${nev}</option>`);
+    }
+
+    this.szuloElem.prepend(valaszto);
+    const cim = $('<h4>Szűrés cég szerint:</h4>');
+    this.szuloElem.prepend(cim);
+    valaszto.on("change", () => this.tablaSzures(valaszto.val()));
+  }
+
+  tablaSzures(nev) {
+    if (!nev) {
+      this.tableElem.find('tbody tr').show();
+      return;
+    }
+    this.tableElem.find('tbody tr').hide().filter(function() {
+      return $(this).find('td').filter(function() {
+        return $(this).text() === nev;
+      }).length > 0;
+    }).show();
+  }
 }
 
 export default AdminKovetelmenyView;
